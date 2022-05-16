5fa776d4291a7ccd3a70a3f1289f330c7a5b8c69
siyixiong <18552541076@163.com>
2022-05-16 23:52:35 +0800

    dev-20220516-video 视频接口编写

--- service/videoService.go
+++ service/videoService.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"mime/multipart"
+	"time"
+)
+
+type Video struct {
+	Id            int64
+	Author        User //todo 等user接口
+	PlayUrl       string
+	CoverUrl      string
+	FavoriteCount int64
+	CommentCount  int64
+	IsFavorite    bool
+}
+
+type VideoService interface {
+	// Feed
+	// 通过传入时间戳，当前用户的id，返回对应的视频数组，以及视频数组中最早的发布时间
+	Feed(lastTime time.Time, userId int64) ([]Video, time.Time, error)
+
+	// GetVideo
+	// 传入视频id获得对应的视频对象
+	GetVideo(videoId int64) (Video, error)
+	// Publish
+	// 将传入的视频流保存在文件服务器中，并存储在mysql表中
+	Publish(data *multipart.FileHeader, userId int64) error
+	// List
+	// 通过userId来查询对应用户发布的视频，并返回对应的视频数组
+	List(userId int64) ([]Video, error)
+}
