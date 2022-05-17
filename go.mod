d41c12f5d4ff546f3ef8bd745726f26cb1e9a4dd
jyd1124 <jiangyudong123@qq.com>
2022-05-17 10:03:27 +0800

    dev-20220517-user userService接口已经实现

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module TikTok
 go 1.18
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
 	github.com/swaggo/swag v1.8.1
--- service/userService.go
+++ service/userService.go
@@ -1,1 +1,44 @@
 package service
+
+type UserService interface {
+	/*
+		个人使用
+	*/
+	// GetTableUserList GetUserList 获得全部TableUser对象
+	GetTableUserList(users *[]TableUser) bool
+
+	// GetTableUserByUsername GetUserByUsername 根据username获得TableUser对象
+	GetTableUserByUsername(name string) bool
+
+	// GetTableUserById GetUserById 根据user_id获得TableUser对象
+	GetTableUserById(id int64) bool
+
+	/*
+		他人使用
+	*/
+	// GetUserById 未登录情况下,根据user_id获得User对象
+	//(调用方法:user.GetUserById,user会被填充;若填充失败,则返回false,成功,返回true)
+	GetUserById(id int64) (User, error)
+
+	// GetUserByIdWithCurId 已登录(curID)情况下,根据user_id获得User对象
+	GetUserByIdWithCurId(id int64, curId int64) (User, error)
+
+	// 根据token返回id
+	// 接口:auth中间件,解析完token,将userid放入context
+	//(调用方法:直接在context内拿参数"user_id"的值)
+}
+
+type TableUser struct {
+	Id       int64
+	Name     string
+	Password string
+}
+
+/*type User struct {
+	Id            int64  `json:"id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   int64  `json:"follow_count,omitempty"`
+	FollowerCount int64  `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
+}
+*/
