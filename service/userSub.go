db7a9a45790285d36056a874470238a9cda2ebda
jyd1124 <jiangyudong123@qq.com>
2022-05-17 19:58:33 +0800

    dev-20220517-user userServiceImpl结构已经实现

--- service/userSub.go
+++ service/userSub.go
@@ -1,1 +1,32 @@
 package service
+
+type FollowServiceImpl struct {
+}
+
+func (fsi *FollowServiceImpl) IsFollowing(userId int64, targetId int64) (bool, error) {
+	return true, nil
+}
+
+func (fsi *FollowServiceImpl) GetFollowingCnt(userId int64) (int64, error) {
+	return int64(1), nil
+}
+
+func (fsi *FollowServiceImpl) GetFollowerCnt(userId int64) (int64, error) {
+	return int64(1), nil
+}
+
+func (fsi *FollowServiceImpl) AddFollowRelation(userId int64, targetId int64) (bool, error) {
+	return true, nil
+}
+
+func (fsi *FollowServiceImpl) DeleteFollowRelation(userId int64, targetId int64) (bool, error) {
+	return true, nil
+}
+
+func (fsi *FollowServiceImpl) GetFollowing(userId int64) ([]User, error) {
+	return nil, nil
+}
+
+func (fsi *FollowServiceImpl) GetFollowers(userId int64) ([]User, error) {
+	return nil, nil
+}
