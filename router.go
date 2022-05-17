300b82b8f6f7945b02b13bc18615ee3366417bee
lzz <1245314855@qq.com>
2022-05-17 19:12:13 +0800

    dev-20220517-like 增加路由

--- router.go
+++ router.go
@@ -8,8 +8,25 @@ import (
 
 func initRouter(r *gin.Engine) {
 	apiRouter := r.Group("/douyin")
+	// basic apis
+	//apiRouter.GET("/feed/", controller.Feed)
+	//apiRouter.GET("/user/", middleware.Auth(), controller.UserInfo)
+	//apiRouter.POST("/user/register/", controller.Register)
+	//apiRouter.POST("/user/login/", controller.Login)
+	//apiRouter.POST("/publish/action/", controller.Publish)
+	//apiRouter.GET("/publish/list/", controller.PublishList)
 
 	apiRouter.GET("/user/", middleware.Auth(), controller.UserInfo)
 	apiRouter.POST("/user/register/", controller.Register)
 	apiRouter.POST("/user/login/", controller.Login)
+	// extra apis - I
+	apiRouter.POST("/favorite/action/", middleware.Auth(), controller.Favorite_Action)
+	apiRouter.GET("/favorite/list/", middleware.Auth(), controller.GetFavouriteList)
+	//apiRouter.POST("/comment/action/", controller.CommentAction)
+	//apiRouter.GET("/comment/list/", controller.CommentList)
+
+	// extra apis - II
+	//apiRouter.POST("/relation/action/", controller.RelationAction)
+	//apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	//apiRouter.GET("/relation/follower/list/", controller.FollowerList)
 }
