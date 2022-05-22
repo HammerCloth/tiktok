59c8179854b6d7a61e125837616d416c6abd738e
jyd1124 <jiangyudong123@qq.com>
2022-05-22 09:35:20 +0800

    dev-20220522-user 打开gorm详细log

--- dao/Initdao.go
+++ dao/Initdao.go
@@ -13,6 +13,8 @@ func Init() {
 	//想要正确的处理time.Time,需要带上 parseTime 参数，
 	//要支持完整的UTF-8编码，需要将 charset=utf8 更改为 charset=utf8mb4
 	Db, err = gorm.Open("mysql", "douyin:zjqxy@tcp(43.138.25.60:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	//打开gorm详细日志
+	Db.LogMode(true)
 	if err != nil {
 		log.Panicln("err:", err.Error())
 	}
