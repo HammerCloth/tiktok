deb5f57d1cc41b6b118bbf1e742cbc5bc3955376
jyd1124 <jiangyudong123@qq.com>
2022-05-22 14:29:13 +0800

    dev-20220522-user 修改auth

--- middleware/Auth.go
+++ middleware/Auth.go
@@ -17,7 +17,8 @@ type Response struct {
 // 若用户携带的token正确,解析token,将userId放入上下文context中并放行;否则,返回错误信息
 func Auth() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		auth := context.Request.Header.Get("Authorization")
+		//auth := context.Request.Header.Get("Authorization")
+		auth := context.Query("token")
 		if len(auth) == 0 {
 			context.Abort()
 			context.JSON(http.StatusUnauthorized, Response{
