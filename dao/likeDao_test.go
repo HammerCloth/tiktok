f16f03a79bcaf6cfdb17e1368ce1328a2fa7047d
lzz <1245314855@qq.com>
2022-06-02 00:32:27 +0800

    dev-20220602-like 补充了测试函数

--- dao/likeDao_test.go
+++ dao/likeDao_test.go
@@ -1,1 +1,43 @@
 package dao
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetLikeUserIdList(t *testing.T) {
+	Init()
+	list, err := GetLikeUserIdList(54)
+	fmt.Printf("%v", list)
+	fmt.Printf("%v", err)
+}
+
+func TestUpdateLike(t *testing.T) {
+	Init()
+	err := UpdateLike(3, 54, 0)
+	fmt.Printf("%v", err)
+}
+
+func TestInsertLike(t *testing.T) {
+	Init()
+	err := InsertLike(Like{
+		UserId:  20003,
+		VideoId: 71,
+		Cancel:  0,
+	})
+	fmt.Printf("%v", err)
+}
+
+func TestGetLikeInfo(t *testing.T) {
+	Init()
+	likeInfo, err := GetLikeInfo(3, 71)
+	fmt.Printf("%v", likeInfo)
+	fmt.Printf("%v", err)
+}
+
+func TestGetLikeVideoIdList(t *testing.T) {
+	Init()
+	videoIdList, err := GetLikeVideoIdList(3)
+	fmt.Printf("%v", videoIdList)
+	fmt.Printf("%v", err)
+}
--- service/likeServiceImpl_test.go
+++ service/likeServiceImpl_test.go
@@ -8,12 +8,40 @@ import (
 )
 
 func TestIsFavourite(t *testing.T) {
+	// 初始化数据库
+	dao.Init()
+	// 初始化FTP服务器链接
+	dao.InitFTP()
+	// 初始化SSH
+	middleware.InitSSH()
+
+	// 初始化redis-DB0的连接，follow选择的DB0.
+	middleware.InitRedis()
+	// 初始化rabbitMQ。
+	middleware.InitRabbitMQ()
+	// 初始化Follow的相关消息队列，并开启消费。
+	middleware.InitFollowRabbitMQ()
+	// 初始化Like的相关消息队列，并开启消费。
 	impl := LikeServiceImpl{}
 	isFavourite, _ := impl.IsFavourite(666, 3)
 	fmt.Printf("%v", isFavourite)
 }
 
 func TestFavouriteCount(t *testing.T) {
+	// 初始化数据库
+	dao.Init()
+	// 初始化FTP服务器链接
+	dao.InitFTP()
+	// 初始化SSH
+	middleware.InitSSH()
+
+	// 初始化redis-DB0的连接，follow选择的DB0.
+	middleware.InitRedis()
+	// 初始化rabbitMQ。
+	middleware.InitRabbitMQ()
+	// 初始化Follow的相关消息队列，并开启消费。
+	middleware.InitFollowRabbitMQ()
+	// 初始化Like的相关消息队列，并开启消费。
 	impl := LikeServiceImpl{}
 	count, _ := impl.FavouriteCount(666)
 	fmt.Printf("%v", count)
